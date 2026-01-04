425e20cf3916c8a192550d87c29c673f82f527dc
Somya Pathak <pathaksoumya2005@gmail.com>
2026-01-04 13:52:40 +0530

    Added complete DSA solutions with comments

--- Linked List/rev_linked_list.cpp
+++ Linked List/rev_linked_list.cpp
@@ -1,3 +1,11 @@
+/*
+Problem:
+Reverse a singly linked list.
+
+Approach:
+Use three pointers (prev, curr, next) to reverse
+the direction of links iteratively. */
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,13 +15,32 @@ struct Node {
     Node(int x) : val(x), next(NULL) {}
 };
 
-Node* reverseList(Node* head) {
+Node* reverseLinkedList(Node* head) {
     Node* prev = NULL;
-    while(head) {
-        Node* next = head->next;
-        head->next = prev;
-        prev = head;
-        head = next;
+    Node* curr = head;
+
+    while (curr != NULL) {
+        Node* nextNode = curr->next; // store next
+        curr->next = prev;           // reverse link
+        prev = curr;                 // move prev
+        curr = nextNode;             // move curr
     }
     return prev;
+}
+
+int main() {
+    // Creating linked list: 1 -> 2 -> 3 -> 4
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+
+    head = reverseLinkedList(head);
+
+    // Printing reversed list
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    return 0;
 }
